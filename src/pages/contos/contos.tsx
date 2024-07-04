27054fc2fcefca3bfcf0744ea3ef004af8631223
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-07-04 07:11:36 -0300

    modify style

--- src/pages/contos/contos.tsx
+++ src/pages/contos/contos.tsx
@@ -1,9 +1,10 @@
-import { Card } from '../../components/card'
 import style from './contos.module.css'
 
 export const Contos = () => {
   return (
     <section className={style.container}>
+
+      <h1>Contos</h1>
     </section>
   )
 }
