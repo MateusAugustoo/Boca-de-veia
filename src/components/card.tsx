c0d4cb6f683249cf3550dbb3db6a0fbcfb4a3f64
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-07-11 18:15:03 -0300

    refectory card functions

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,29 +1,23 @@
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR'
 import style from './card.module.css'
+import { ConstosProps as CardProps } from '../interface/contosI'
 
-interface CardProps {
-  category: string
-  content: string
-  time: Date
-}
 
-export const Card = ({ category, content, time }: CardProps) => {
+export const Card = ({ category, message, time }: CardProps) => {
 
-  const published = time.toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric' })
-  const publishedDateRelativeToNow = formatDistanceToNow(time, { addSuffix: true, locale: ptBR })
+  const publishDate = time.toDate()
+  const formattedDate = formatDistanceToNow(publishDate, {addSuffix:true, locale:ptBR})
 
   const iconMap: Record<string, string> = {
     mensagem: '✉️',
     denuncia: '⚠️',
-    sugestao: '💡',
     fofoca: '🤔',
   }
 
   const colorMap: Record<string, string> = {
     mensagem: 'message',
     denuncia: 'warning',
-    sugestao: 'suggestion',
     fofoca: 'question',
   }
   const icon = iconMap[category]
@@ -40,12 +34,8 @@ export const Card = ({ category, content, time }: CardProps) => {
       </div>
 
       <div className={style.container__content}>
-        <p>{content}</p>
-        <time
-          title={published}
-          dateTime={time.toISOString()}>
-          {publishedDateRelativeToNow}
-        </time>
+        <p>{message}</p>
+        <small>{formattedDate}</small>
       </div>
     </div>
   );
